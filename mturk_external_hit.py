e4f3adb044f981a2295ee94ac5e57a90f5c3bf1b
Sai-V <bhargavi.kpf229@gmail.com>
2017-02-03 01:16:55 -0500

    Update mturk_external_hit.py

--- mturk_external_hit.py
+++ mturk_external_hit.py
@@ -5,8 +5,8 @@ sandbox_host = 'mechanicalturk.sandbox.amazonaws.com'
 real_host = 'mechanicalturk.amazonaws.com'
 
 mturk = boto.mturk.connection.MTurkConnection(
-    aws_access_key_id = 'AKIAJN3NC726HKVYAMHQ',
-    aws_secret_access_key = '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',
+    aws_access_key_id = 'XXX',
+    aws_secret_access_key = 'XXX',
     host = sandbox_host,
     debug = 1 # debug = 2 prints out all requests. but we'll just keep it at 1
 )
